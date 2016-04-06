73009cc079f651cbabc92661f5d4175f0200d1cf
David Babineau <babineaudavid@gmail.com>
2016-04-06 09:08:53 -0400

    ruby

--- dog_cat.rb
+++ dog_cat.rb
@@ -1,4 +1,4 @@
-puts 'Is your pet a dog or a cat?'
+puts 'Is your pet a big dog or a cat?'
 
 pet = gets.chomp.downcase
 
--- ruby_ex/age.rb
+++ ruby_ex/age.rb
@@ -2,4 +2,4 @@ puts 'Hello there, and what\'s your favorite number?'
 numFAV = gets.chomp
 numBET = numFAV.to_i + 1
 puts ''
-puts 'Hello there but, number ' + numFAV.to_s + ' would be better plus 1...dont you think? ' + numBET.to_s + ' is a sweet pussy!'
+puts 'Hello there but, number ' + numFAV.to_s + ' would be better plus 1...dont you think? ' + numBET.to_s + ' is a sweet!'
